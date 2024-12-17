af532db3624672230f4ccf951f5d3aa157199c0a
minkyung kim <minry20415@naver.com>
2024-12-17 16:18:24 +0900

    ✨: 여러 가지 요청 매핑들
    - 경로 변수 @PathVariable(다중 사용 가능)
    - params = "" URL 쿼리로 ""<내에 있는 문자가 넘어와야 호출
    - headers = "" header에 ""<내에 있는 키,값이 넘어와야 호출
    - consumes = "" Content-Tpye이 ""와 같은것 다른것 등만 호출
    - produces = "" Accept이 ""와 같은것 다른것 등만 호출

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,84 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class MappingController {
+
+    @GetMapping("/mapping-get-v2")
+    public String helloBasic() {
+        log.info("hello-basic");
+        return "ok";
+    }
+
+    /**
+     * 경로 변수 @PathVariable 사용
+     * 변수명이 같은면 생략 가능
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     * /mapping/userA
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    /**
+     * 경로 변수 @PathVariable 다중 사용
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable("userId") String userId, @PathVariable("orderId") String orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * mode=debug라는 특정 파라미터 값이 없으면 호출이 안됨
+     * 여러가지로 표현 가능
+     * params="mode"
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug"
+     * params={"mode=debug", "data=good"}
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsume() {
+        log.info("mappingConsume");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * produces="text/html"
+     * produces="!text/html"
+     * produces="text/*"
+     * produces="*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
